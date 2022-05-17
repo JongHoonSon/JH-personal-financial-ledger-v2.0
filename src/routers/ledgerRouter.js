3de2e2012dc9ff35d87364f3e842d28c0887b1c4
JongHoonSon <roka1684@gmail.com>
2022-05-18 01:11:21 +0900

    Add "/" in ledgerRouter

--- src/routers/ledgerRouter.js
+++ src/routers/ledgerRouter.js
@@ -15,14 +15,14 @@ import {
 
 const ledgerRouter = express.Router();
 
-ledgerRouter.get(":yyyy/:mm", getLedger);
-ledgerRouter.route("add-item").get(getAddItem).post(postAddItem);
-ledgerRouter.route("edit-item/:itemId").get(getEditItem).post(postEditItem);
-ledgerRouter.post("delete-item/:itemId", postDeleteItem);
-ledgerRouter.post("delete-items/:itemIds", postDeleteItems);
-ledgerRouter.get("detail-item/:itemId", getDetailItem);
-ledgerRouter.get("chart", getChart);
-ledgerRouter.post("add-pin/:itemId", postAddPin);
-ledgerRouter.post("remove-pin/:itemId", postRemovePin);
+ledgerRouter.get("/:yyyy([0-9]{4})-:mm([0-9]{2})", getLedger);
+ledgerRouter.route("/add-item").get(getAddItem).post(postAddItem);
+ledgerRouter.route("/edit-item/:itemId").get(getEditItem).post(postEditItem);
+ledgerRouter.post("/delete-item/:itemId", postDeleteItem);
+ledgerRouter.post("/delete-items/:itemIds", postDeleteItems);
+ledgerRouter.get("/detail-item/:itemId", getDetailItem);
+ledgerRouter.get("/chart", getChart);
+ledgerRouter.post("/add-pin/:itemId", postAddPin);
+ledgerRouter.post("/remove-pin/:itemId", postRemovePin);
 
 export default ledgerRouter;
