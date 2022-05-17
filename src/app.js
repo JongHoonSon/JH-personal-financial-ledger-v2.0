f55ff96c36292973f24ee16632b9dca4b91a749f
JongHoonSon <roka1684@gmail.com>
2022-05-17 21:44:45 +0900

    Set express server

---
+++ src/app.js
@@ -0,0 +1,13 @@
+let express = require("express");
+
+let app = express();
+
+const port = app.listen(5050);
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello</h1>");
+});
+
+app.listen(port, function () {
+  console.log(`express server start! http://localhost:5050/`);
+});
