4ba982a82a044214742eb3b533639172f49f0dac
JongHoonSon <roka1684@gmail.com>
2022-05-17 23:59:39 +0900

    Add Routers

---
+++ src/routers/ledgerRouter.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const ledgerRouter = express.Router();
+
+export default ledgerRouter;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 import express from "express";
+import ledgerRouter from "./routers/ledgerRouter";
+import rootRouter from "./routers/rootRouter";
+import userRouter from "./routers/userRouter";
 
 const app = express();
+
+app.use("/", rootRouter);
+app.use("/user", userRouter);
+app.use("/ledger", ledgerRouter);
 
 export default app;
