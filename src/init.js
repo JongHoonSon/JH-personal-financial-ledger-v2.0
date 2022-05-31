9d72277f2e16e055204b8b376fd576a295a3e4da
JongHoonSon <roka1684@gmail.com>
2022-05-31 17:37:17 +0900

    Edit port number 4000 -> 4001

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import "./db";
 import app from "./server";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 
 const handleListening = () =>
   console.log(`express server start! http://localhost:${PORT}/`);
