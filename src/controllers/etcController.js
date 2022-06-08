ba072e9c7be745280c5466ad745cf5161ce4ede9
JongHoonSon <roka1684@gmail.com>
2022-06-08 18:12:13 +0900

    Add Controller and Router for 'last expense'

--- src/controllers/etcController.js
+++ src/controllers/etcController.js
@@ -1,3 +1,7 @@
 export const getChart = (req, res) => {
   res.render("etc/chart", { pageTitle: "Chart" });
 };
+
+export const getLastExpense = (req, res) => {
+  res.render("etc/lastExpense", { pageTitle: "Last Expense" });
+};
--- src/routers/etcRouter.js
+++ src/routers/etcRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getChart } from "../controllers/etcController";
+import { getChart, getLastExpense } from "../controllers/etcController";
 
 const etcRouter = express.Router();
 
 etcRouter.get("/chart", getChart);
+etcRouter.get("/last-expense", getLastExpense);
 
 export default etcRouter;
