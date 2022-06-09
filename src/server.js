2bc544f5147da4b0d43cc2307478497b3d77d6ac
JongHoonSon <roka1684@gmail.com>
2022-06-09 23:04:11 +0900

    Install & Use morgan logger and Use express.urlencoded for POST method form

--- src/server.js
+++ src/server.js
@@ -4,14 +4,19 @@ import itemRouter from "./routers/itemRouter";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import etcRouter from "./routers/etcRouter";
+import morgan from "morgan";
 
 const app = express();
+const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
 app.use("/assets", express.static("assets"));
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/user", userRouter);
 app.use("/item", itemRouter);
