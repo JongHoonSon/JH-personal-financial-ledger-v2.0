a25f2ad19519c4a3eb987d9c12393f884650469b
JongHoonSon <roka1684@gmail.com>
2022-06-17 22:12:11 +0900

    Edit session options

--- src/server.js
+++ src/server.js
@@ -22,9 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
