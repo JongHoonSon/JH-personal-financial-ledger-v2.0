da0f317fe9db9991906442847d9fa07c55eb823d
JongHoonSon <roka1684@gmail.com>
2022-07-06 23:45:48 +0900

    Add default value for income createAt

--- src/models/Income.js
+++ src/models/Income.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const incomeSchema = new mongoose.Schema({
   owner: { type: mongoose.Schema.Types.ObjectId, require: true, ref: "User" },
   date: { type: Date, required: true },
-  createdAt: { type: Date, required: true },
+  createdAt: { type: Date, required: true, default: Date.now },
   amount: { type: Number, required: true },
   description: { type: String },
   category: { type: String, required: true },
