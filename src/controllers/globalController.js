bc91b9a636beec245eb28483e3a020f1c5c1c449
JongHoonSon <roka1684@gmail.com>
2022-07-07 19:11:21 +0900

    Refactoring

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -13,26 +13,25 @@ export const getJoin = (req, res) => {
 export const postJoin = async (req, res) => {
   const { username, password, password_confirm, name, email, nickname } =
     req.body;
-  const checkUsername = await User.exists({ username });
-  const checkEmail = await User.exists({ email });
-  const checkNickname = await User.exists({ nickname });
-
-  console.log(req.body);
 
   if (password !== password_confirm) {
     req.flash("error", "Password confirm failed.");
     return res.status(400).render("global/join", { pageTitle: "Join" });
   }
+
+  const checkUsername = await User.exists({ username });
   if (checkUsername === true) {
     req.flash("error", "This username is already taken");
     return res.status(400).render("global/join", { pageTitle: "Join" });
   }
 
+  const checkEmail = await User.exists({ email });
   if (checkEmail === true) {
     req.flash("error", "This email is already taken");
     return res.status(400).render("global/join", { pageTitle: "Join" });
   }
 
+  const checkNickname = await User.exists({ nickname });
   if (checkNickname === true) {
     req.flash("error", "This nickname is already taken");
     return res.status(400).render("global/join", { pageTitle: "Join" });
@@ -46,13 +45,13 @@ export const postJoin = async (req, res) => {
       email,
       nickname,
     });
+    req.flash("success", "User created.");
+    return res.redirect("/login");
   } catch (error) {
+    console.log(error);
     req.flash("error", "An error occurred while creating a user.");
     return res.status(400).render("global/join", { pageTitle: "Join" });
   }
-
-  req.flash("success", "User created.");
-  return res.redirect("/login");
 };
 
 export const getLogin = (req, res) => {
@@ -63,14 +62,12 @@ export const postLogin = async (req, res) => {
   const { username, password } = req.body;
 
   const user = await User.findOne({ username, socialOnly: false });
-
   if (!user) {
     req.flash("error", "User does not exists.");
     return res.status(400).render("global/login", { pageTitle: "Login" });
   }
 
   const passwordCorrect = await bcrypt.compare(password, user.password);
-
   if (!passwordCorrect) {
     req.flash("error", "Incorrect password.");
     return res.status(400).render("global/login", { pageTitle: "Login" });
@@ -78,6 +75,7 @@ export const postLogin = async (req, res) => {
 
   req.session.loggedIn = true;
   req.session.user = user;
+
   req.flash("success", `Hello, ${user.nickname} !`);
   return res.redirect("/");
 };
@@ -87,6 +85,7 @@ export const logout = (req, res) => {
   req.session.user = null;
   req.session.loggedIn = false;
   res.locals.loggedInUser = req.session.user;
+
   req.flash("success", `See you again, ${loggedInUserNickname}!`);
   return res.redirect("/");
 };
