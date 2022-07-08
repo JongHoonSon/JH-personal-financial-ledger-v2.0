6c231ed8de0678eade5429a5d4a38291b37f735a
JongHoonSon <roka1684@gmail.com>
2022-07-08 20:53:12 +0900

    Add now date to res.locals

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,10 @@
 export const localMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user || {};
+  const date = new Date();
+  res.locals.thisYear = date.getFullYear();
+  res.locals.thisMonth = date.getMonth().toString().padStart(2, 0);
+  res.locals.thisDay = date.getDate().toString().padStart(2, 0);
   console.log(res.locals);
   next();
 };
