c84d1ff2c1b40ae1482ed85ec81197f403080785
JongHoonSon <roka1684@gmail.com>
2022-07-11 23:07:55 +0900

    Edit res.locals.thisYear's type number to string

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,7 +2,7 @@ export const localMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user || {};
   const date = new Date();
-  res.locals.thisYear = date.getFullYear();
+  res.locals.thisYear = date.getFullYear().toString();
   res.locals.thisMonth = (date.getMonth() + 1).toString().padStart(2, 0);
   res.locals.thisDay = date.getDate().toString().padStart(2, 0);
   console.log(res.locals);
