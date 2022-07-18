ffdc9afa0a981b1fa950d3f38e56e0c28befdff2
JongHoonSon <roka1684@gmail.com>
2022-07-18 23:05:08 +0900

    Implement getEditProfile and \postEditProfile controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,10 +10,59 @@ export const getProfile = async (req, res) => {
   res.render("user/profile", { pageTitle: "프로필", user, isMyProfile });
 };
 
-export const getEditProfile = (req, res) => {
+export const getEditProfile = async (req, res) => {
+  const { userId } = req.params;
+
+  const loggedInUser = res.locals.loggedInUser;
+
+  const user = await User.findById(userId);
+
+  if (String(loggedInUser._id) !== String(user._id)) {
+    req.flash("error", "권한이 없습니다.");
+    return res.status(403).redirect("/");
+  }
+
   res.render("user/editProfile", {
     pageTitle: "프로필 수정",
+    user,
   });
 };
 
-export const postEditProfile = (req, res) => {};
+export const postEditProfile = async (req, res) => {
+  const { userId } = req.params;
+  const { username, name, nickname, email } = req.body;
+
+  const loggedInUser = res.locals.loggedInUser;
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    req.flash("error", "유저를 찾을 수 없습니다.");
+    return res.status(404).redirect("/");
+  }
+
+  if (String(loggedInUser._id) !== String(user._id)) {
+    req.flash("error", "권한이 없습니다.");
+    return res.status(403).redirect("/");
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        username,
+        name,
+        nickname,
+        email,
+      },
+      { new: true }
+    );
+    req.flash("success", "프로필을 수정했습니다.");
+    req.session.user = updatedUser;
+    return res.status(200).redirect(`/user/profile/${userId}`);
+  } catch (error) {
+    console.log(error);
+    req.flash("error", "프로필을 수정하는 과정에서 오류가 발생했습니다.");
+    return res.status(400).redirect(`/user/profile/${userId}`);
+  }
+};
