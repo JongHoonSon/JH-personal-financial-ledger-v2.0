06ef37d32858fad0e13e6b69f6fa9cf8d95172fe
JongHoonSon <roka1684@gmail.com>
2022-07-22 23:51:19 +0900

    Add title property to Post

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
   category: { type: String, required: true },
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   createdAt: { type: Date, required: true, default: Date.now },
