d745da286d3f1a981d47e8b76dd18085e50169d2
JongHoonSon <roka1684@gmail.com>
2022-07-22 23:51:32 +0900

    Implement postAddPost controller

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,3 +1,6 @@
+import User from "../models/User";
+import Post from "../models/Post";
+
 export const getBoard = (req, res) => {
   res.status(200).render("board/board", { pageTitle: "게시판" });
 };
@@ -6,11 +9,31 @@ export const getAddPost = (req, res) => {
   res.status(200).render("board/addPost", { pageTitle: "글 작성" });
 };
 
-export const postAddPost = (req, res) => {
+export const postAddPost = async (req, res) => {
   const { title, category, content } = req.body;
 
   console.log("title, category, content");
   console.log(title, category, content);
+
+  const loggedInUser = req.session.user;
+  const user = await User.findById(loggedInUser._id);
+
+  try {
+    const newPost = await Post.create({
+      title,
+      category,
+      owner: user,
+      content,
+    });
+    user.postList.push(newPost);
+    user.save();
+    req.flash("success", "글을 작성하였습니다.");
+    return res.status(200).redirect("/board/add-post");
+  } catch (error) {
+    console.log(error);
+    req.flash("error", "글을 작성하는 과정에서 오류가 발생했습니다.");
+    return res.status(400).redirect("/board/add-post");
+  }
 
   res.status(200).render("board/addPost", { pageTitle: "글 작성" });
 };
