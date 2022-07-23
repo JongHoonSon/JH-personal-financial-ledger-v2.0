011810d5984dd750be1d2603f8bd2c5b8032d5f1
JongHoonSon <roka1684@gmail.com>
2022-07-23 16:53:44 +0900

    Use mongoose-auto-increment to auto increase Post's seq prop

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,16 +1,34 @@
 import mongoose from "mongoose";
+import autoIncrement from "mongoose-auto-increment";
 
-const postSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  category: { type: String, required: true },
-  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-  createdAt: { type: Date, required: true, default: Date.now },
-  content: { type: String, required: true },
-  views: { type: Number, required: true, default: 0 },
-  likes: { type: Number, required: true, default: 0 },
-  commentList: [
-    { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" },
-  ],
+autoIncrement.initialize(mongoose.connection);
+
+const postSchema = new mongoose.Schema(
+  {
+    seq: { type: Number, required: true, default: 0 },
+    title: { type: String, required: true },
+    category: { type: String, required: true },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    createdAt: { type: Date, required: true, default: Date.now },
+    content: { type: String, required: true },
+    views: { type: Number, required: true, default: 0 },
+    likes: { type: Number, required: true, default: 0 },
+    commentList: [
+      { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" },
+    ],
+  },
+  { collection: "", versionKey: false }
+);
+
+postSchema.plugin(autoIncrement.plugin, {
+  model: "post",
+  field: "seq",
+  startAt: 1,
+  increment: 1,
 });
 
 const Post = mongoose.model("Post", postSchema);
