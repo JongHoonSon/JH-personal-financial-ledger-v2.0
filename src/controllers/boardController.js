9874c0a7d8e5a1b0129a3484dc44bbf8a128cc45
JongHoonSon <roka1684@gmail.com>
2022-07-26 00:06:48 +0900

    Refactor getBoard Controller

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -7,31 +7,44 @@ export const getBoard = async (req, res) => {
     path: "postList",
     populate: { path: "board" },
   });
-  const postList = board.postList;
+  const totalPostList = board.postList;
 
-  let thisPageNum = Number(pageNum);
-  let firstPageNum = 1;
-  let lastPageNum = Math.ceil(postList.length / 10);
+  let emptyFlag;
+  let thisPageNum;
+  let firstPageNum;
+  let lastPageNum;
+  let postList = [];
 
-  let emptyFlag = false;
-
-  if (postList.length === 0) {
+  if (totalPostList.length === 0) {
     emptyFlag = true;
     thisPageNum = Number(pageNum);
     firstPageNum = 1;
     lastPageNum = 1;
   } else {
+    emptyFlag = false;
     thisPageNum = Number(pageNum);
     firstPageNum = 1;
-    lastPageNum = Math.ceil(postList.length / 10);
+    lastPageNum = Math.ceil(totalPostList.length / 10);
+
+    totalPostList.reverse();
+
+    console.log("totalPostList");
+    console.log(totalPostList);
+
+    for (let i = 0; i <= 9; i++) {
+      if (totalPostList.length === (thisPageNum - 1) * 10 + i) {
+        break;
+      }
+      postList.push(totalPostList[(thisPageNum - 1) * 10 + i]);
+    }
   }
 
   if (thisPageNum < firstPageNum || thisPageNum > lastPageNum) {
     req.flash("error", "잘못된 접근입니다.");
-    res.status(404).redirect(`/board/${boardName}/1`);
+    return res.status(404).redirect(`/board/${boardName}/1`);
   }
 
-  res.status(200).render("board/board", {
+  return res.status(200).render("board/board", {
     pageTitle: "게시판",
     postList,
     boardName,
