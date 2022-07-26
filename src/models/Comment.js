f2cecb7951f497e731163ae6e607841c26643253
JongHoonSon <roka1684@gmail.com>
2022-07-26 21:50:56 +0900

    Add stringDate prop in Comment

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { getStringDate } from "../utils";
 
 const commentSchema = new mongoose.Schema({
   owner: {
@@ -7,8 +8,13 @@ const commentSchema = new mongoose.Schema({
     ref: "User",
   },
   createdAt: { type: Date, required: true, default: Date.now },
+  stringDate: {
+    type: String,
+    required: true,
+    default: getStringDate(new Date()),
+  },
   content: { type: String, required: true },
-  likes: { type: Number, required: true, defualt: 0 },
+  likes: { type: Number, required: true, default: 0 },
   post: {
     type: mongoose.Schema.Types.ObjectId,
     require: true,
