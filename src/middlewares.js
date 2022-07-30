1a4fc1054f2ccefc378af37e9bd3346f9715b47a
JongHoonSon <roka1684@gmail.com>
2022-07-31 02:11:20 +0900

    Edit createObjectMiddleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,33 +4,28 @@ import Board from "./models/Board";
 export const uploadFiles = multer({ dest: "uploads/" });
 
 export const createObjectMiddleware = async (req, res, next) => {
-  const boardList = ["수다", "질문", "꿀팁공유", "핫딜", "기타"];
-  res.locals.boardList = boardList;
+  const boardList = ["전체게시판", "수다", "질문", "꿀팁공유", "핫딜", "기타"];
 
-  let totalBoardExists;
-  try {
-    totalBoardExists = await Board.exists({ name: "전체게시판" });
-  } catch (error) {
-    console.log(error);
-    req.flash(
-      "error",
-      "전체게시판을 존재하는지 확인 과정에서 오류가 발생했습니다."
-    );
-  }
-  if (!totalBoardExists) {
+  for (let i = 0; i < boardList.length; i++) {
+    let boardExists;
     try {
-      await Board.create({ name: "전체게시판" });
+      boardExists = await Board.exists({ name: boardList[i] });
     } catch (error) {
       console.log(error);
-      req.flash("error", "전체게시판을 생성하는 과정에서 오류가 발생했습니다.");
+      req.flash(
+        "error",
+        `${boardList[i]} 게시판을 불러오는 과정에서 오류가 발생했습니다.`
+      );
     }
-
-    for (let i = 0; i < boardList.length; i++) {
+    if (!boardExists) {
       try {
         await Board.create({ name: boardList[i] });
       } catch (error) {
         console.log(error);
-        req.flash("error", "게시판을 생성하는 과정에서 오류가 발생했습니다.");
+        req.flash(
+          "error",
+          `${boardList[i]} 게시판을 생성하는 과정에서 오류가 발생했습니다.`
+        );
       }
     }
   }
@@ -47,6 +42,8 @@ export const localMiddleware = (req, res, next) => {
   res.locals.thisYear = date.getFullYear().toString();
   res.locals.thisMonth = (date.getMonth() + 1).toString().padStart(2, 0);
   res.locals.thisDay = date.getDate().toString().padStart(2, 0);
+
+  res.locals.boardList = ["수다", "질문", "꿀팁공유", "핫딜", "기타"];
 
   next();
 };
