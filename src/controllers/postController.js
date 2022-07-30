f43ff77adb0ae4b2cff42abfabb3b030da349bcd
JongHoonSon <roka1684@gmail.com>
2022-07-30 15:32:39 +0900

    Implement get/post editPost Controller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,6 +1,7 @@
 import User from "../models/User";
 import Board from "../models/Board";
 import Post from "../models/Post";
+import { unauthorizedAccess } from "../middlewares";
 
 export const getAddPost = (req, res) => {
   res.status(200).render("post/addPost", { pageTitle: "글 작성" });
@@ -37,11 +38,57 @@ export const postAddPost = async (req, res) => {
   }
 };
 
-export const getEditPost = (req, res) => {
-  res.status(200).render("post/editPost", { pageTitle: "글 수정" });
+export const getEditPost = async (req, res) => {
+  const { postId } = req.params;
+
+  const checkResult = await checkPostOwnerIsLoggedInUser(req, res, postId);
+
+  if (checkResult.pass === false) {
+    return checkResult.return;
+  }
+
+  const post = checkResult.post;
+
+  res.status(200).render("post/editPost", { pageTitle: "게시글 수정", post });
 };
 
-export const postEditPost = async (req, res) => {};
+export const postEditPost = async (req, res) => {
+  const { postId } = req.params;
+  const { title, boardName, content } = req.body;
+
+  const checkResult = await checkPostOwnerIsLoggedInUser(req, res, postId);
+
+  if (checkResult.pass === false) {
+    return checkResult.return;
+  }
+
+  const post = checkResult.post;
+
+  try {
+    post.board.postList = post.board.postList.filter((el) => {
+      if (String(el._id) !== String(post.id)) {
+        return true;
+      }
+    });
+    await post.board.save();
+
+    const board = await Board.findOne({ name: boardName });
+    board.postList.push(post);
+    await board.save();
+
+    post.title = title;
+    post.board = board;
+    post.content = content;
+    await post.save();
+
+    req.flash("success", "게시글을 수정했습니다.");
+    return res.status(200).redirect(`/post/detail/${postId}`);
+  } catch (error) {
+    console.log(error);
+    req.flash("error", "게시글을 수정하는 과정에서 오류가 발생했습니다.");
+    return res.redirect(`/post/edit/${postId}`);
+  }
+};
 
 export const postDeletePost = async (req, res) => {};
 
@@ -117,4 +164,27 @@ export const postIncreaseLikesPost = async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+const checkPostOwnerIsLoggedInUser = async (req, res, postId) => {
+  const post = await Post.findById(postId).populate({
+    path: "board",
+    populate: "postList",
+  });
+
+  if (!post) {
+    req.flash("error", "게시글을 찾을 수 없습니다.");
+    return { pass: false, return: res.status(404).redirect("/") };
+  }
+
+  const loggedInUser = req.session.user;
+  const user = await User.findById(loggedInUser._id);
+
+  console.log(String(post.owner._id) !== String(user._id));
+
+  if (String(post.owner._id) !== String(user._id)) {
+    return { pass: false, return: unauthorizedAccess(req, res) };
+  }
+
+  return { pass: true, post: post, user: user };
 };
