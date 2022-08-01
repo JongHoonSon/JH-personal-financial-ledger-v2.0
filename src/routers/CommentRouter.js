92416d69109eb9a18f9c94e107cb1715e582fc61
JongHoonSon <roka1684@gmail.com>
2022-08-02 00:10:17 +0900

    Implement postEditComment, postDeleteComment and postIncreaseLikesComment Controllers

--- src/routers/CommentRouter.js
+++ src/routers/CommentRouter.js
@@ -1,9 +1,28 @@
 import express from "express";
 import { loggedInUserOnly } from "../middlewares";
-import { postAddComment } from "../controllers/commentController";
+import {
+  postAddComment,
+  postDeleteComment,
+  postEditComment,
+  postIncreaseLikesComment,
+} from "../controllers/commentController";
 
 const commentRouter = express.Router();
 
 commentRouter.post("/add/:postId", loggedInUserOnly, postAddComment);
+commentRouter.post(
+  "/edit/:postId/:commentId",
+  loggedInUserOnly,
+  postEditComment
+);
+commentRouter.post(
+  "/delete/:postId/:commentId",
+  loggedInUserOnly,
+  postDeleteComment
+);
+commentRouter.post(
+  "/increase-likes/:postId/:commentId",
+  postIncreaseLikesComment
+);
 
 export default commentRouter;
