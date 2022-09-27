1f366e4484e097b7a679053164a66f72f5660034
JongHoonSon <roka1684@gmail.com>
2022-09-27 19:30:59 +0900

    Edit addItem

--- src/client/js/imagePreview.js
+++ src/client/js/imagePreview.js
@@ -1,15 +1,17 @@
-const imageUpload = document.querySelector(".image-upload");
+const imageUpload = document.getElementById("add-item-input__image-upload");
 
 function handleImagePreview() {
   if (imageUpload.files && imageUpload.files[0]) {
     const reader = new FileReader();
     reader.onload = function (event) {
-      document.querySelector(".image-preview").src = event.target.result;
+      document.getElementById("add-item-input__image-preview").src =
+        event.target.result;
     };
     reader.readAsDataURL(imageUpload.files[0]);
   } else {
     const originImageUrl = imageUpload.dataset.id;
-    document.querySelector(".image-preview").src = "/" + originImageUrl;
+    document.getElementById("add-item-input__image-preview").src =
+      "/" + originImageUrl;
   }
 }
 
