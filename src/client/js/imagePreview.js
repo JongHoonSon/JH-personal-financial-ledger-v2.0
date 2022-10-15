c109878140788458f1c05f9a756315267dc32671
JongHoonSon <roka1684@gmail.com>
2022-10-15 18:08:30 +0900

    Edit addItem file upload input

--- src/client/js/imagePreview.js
+++ src/client/js/imagePreview.js
@@ -1,6 +1,7 @@
 const imageUpload = document.getElementById("add-item-input__image-upload");
 
 function handleImagePreview() {
+  const imageName = document.getElementById("add-item-input__image-name");
   if (imageUpload.files && imageUpload.files[0]) {
     const reader = new FileReader();
     reader.onload = function (event) {
@@ -8,10 +9,13 @@ function handleImagePreview() {
         event.target.result;
     };
     reader.readAsDataURL(imageUpload.files[0]);
+    // console.log(imageName);
+    imageName.value = imageUpload.value;
   } else {
     const originImageUrl = imageUpload.dataset.id;
     document.getElementById("add-item-input__image-preview").src =
       "/" + originImageUrl;
+    imageName.value = "파일첨부";
   }
 }
 
