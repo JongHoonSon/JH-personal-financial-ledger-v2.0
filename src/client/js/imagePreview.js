21fea2faa033d4ff79bfa510e6a199b1239f0b97
JongHoonSon <roka1684@gmail.com>
2022-10-16 23:09:46 +0900

    Separate image-upload component from addItem

--- src/client/js/imagePreview.js
+++ src/client/js/imagePreview.js
@@ -1,18 +1,23 @@
-const imageUpload = document.querySelector(".image-upload");
+const imageUpload = document.getElementById(
+  "image-upload__uploader__image-input"
+);
 
 function handleImagePreview() {
-  const imageName = document.querySelector(".image-name");
+  const imageName = document.getElementById(
+    "image-upload__uploader__image-name"
+  );
   if (imageUpload.files && imageUpload.files[0]) {
     const reader = new FileReader();
     reader.onload = function (event) {
-      document.querySelector(".image-preview").src = event.target.result;
+      document.getElementById("image-upload__preview").src =
+        event.target.result;
     };
     reader.readAsDataURL(imageUpload.files[0]);
     // console.log(imageName);
     imageName.value = imageUpload.value;
   } else {
     const originImageUrl = imageUpload.dataset.id;
-    document.querySelector(".image-preview").src = "/" + originImageUrl;
+    document.getElementById("image-upload__preview").src = "/" + originImageUrl;
     imageName.value = "파일첨부";
   }
 }
