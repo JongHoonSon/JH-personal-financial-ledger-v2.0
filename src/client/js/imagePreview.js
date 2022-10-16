c92afc3eed597d09f4e7abcab15617287d1496d4
JongHoonSon <roka1684@gmail.com>
2022-10-16 22:17:02 +0900

    Edit imagePreview.js to use in several pug

--- src/client/js/imagePreview.js
+++ src/client/js/imagePreview.js
@@ -1,20 +1,18 @@
-const imageUpload = document.getElementById("add-item-input__image-upload");
+const imageUpload = document.querySelector(".image-upload");
 
 function handleImagePreview() {
-  const imageName = document.getElementById("add-item-input__image-name");
+  const imageName = document.querySelector(".image-name");
   if (imageUpload.files && imageUpload.files[0]) {
     const reader = new FileReader();
     reader.onload = function (event) {
-      document.getElementById("add-item-input__image-preview").src =
-        event.target.result;
+      document.querySelector(".image-preview").src = event.target.result;
     };
     reader.readAsDataURL(imageUpload.files[0]);
     // console.log(imageName);
     imageName.value = imageUpload.value;
   } else {
     const originImageUrl = imageUpload.dataset.id;
-    document.getElementById("add-item-input__image-preview").src =
-      "/" + originImageUrl;
+    document.querySelector(".image-preview").src = "/" + originImageUrl;
     imageName.value = "파일첨부";
   }
 }
