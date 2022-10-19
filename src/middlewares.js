bb40d4b926c817d37d2a651afbb43a4ab0dd050b
JongHoonSon <roka1684@gmail.com>
2022-10-19 10:11:30 +0900

    Edit logHistory middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -74,7 +74,7 @@ export const unauthorizedAccess = (req, res, next) => {
 // html 문서를 가져오는 GET 요청에서만 방문한 URL을 세션의 logHistory에 기록하는 미들웨어
 export const logHistory = (req, res, next) => {
   if (req.method === "GET" && req.headers["sec-fetch-dest"] === "document") {
-    if (!req.session.logHistory.prev1) {
+    if (!req.session.logHistory) {
       req.session.logHistory = {
         prev1: req.path,
       };
