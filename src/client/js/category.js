4ec453e9d8f19df21993f2b858d68097536e0dee
JongHoonSon <roka1684@gmail.com>
2022-10-21 17:45:29 +0900

    Implement delete user category func

--- src/client/js/category.js
+++ src/client/js/category.js
@@ -63,29 +63,30 @@ sumbitButtonIcon.addEventListener("click", async (e) => {
   categoryInput.value = "";
 });
 
-// const categoryList = document.getElementById("user-category-list");
-// const categoryDeleteBtns = document.querySelectorAll(".category-delete-btn");
+const categoryDeleteBtnIcons = document.querySelectorAll(
+  ".user-own-categories__category__del-btn__icon"
+);
 
-// const categoryType = categoryList.dataset.categorytype;
+const handleDeleteCategory = async (event) => {
+  const selectedTab = document.querySelector(".selected-tab");
 
-// const handleCategoryDeleteBtnClick = async (event) => {
-//   const categoryName = event.target.dataset.category;
-//   console.log("categoryName");
-//   console.log(categoryName);
+  const categoryType = selectedTab.dataset.categorytype;
 
-//   const response = await fetch(`/user/delete/category/${categoryType}`, {
-//     method: "DELETE",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ categoryName }),
-//   });
+  const categoryName = event.target.dataset.category;
 
-//   if (response.status === 200) {
-//     window.location.reload();
-//   }
-// };
+  const response = await fetch(`/user/delete/category/${categoryType}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ categoryName }),
+  });
 
-// categoryDeleteBtns.forEach((btn) => {
-//   btn.addEventListener("click", handleCategoryDeleteBtnClick);
-// });
+  if (response.status === 200) {
+    console.log("delete category success");
+  }
+};
+
+categoryDeleteBtnIcons.forEach((el) => {
+  el.addEventListener("click", handleDeleteCategory);
+});
