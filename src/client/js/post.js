ec82dbae22e3302b2c274520a0b4ee4f1676776f
JongHoonSon <roka1684@gmail.com>
2022-12-02 18:46:16 +0900

    Update: Edit post.js code

--- src/client/js/post.js
+++ src/client/js/post.js
@@ -1,10 +1,41 @@
-const detailPost = document.getElementById("detailPost");
+const detailPost = document.getElementById("detail-post");
+const { post_id } = detailPost.dataset;
 
 const increaseView = () => {
-  const { post_id } = detailPost.dataset;
-  fetch(`/post/increase-views/${post_id}/`, {
+  fetch(`/post/increase-views/${post_id}`, {
     method: "POST",
   });
 };
 
 increaseView();
+
+const postLikeButton = document.getElementById("detail-post__like-button");
+
+if (postLikeButton) {
+  const increateLike = () => {
+    fetch(`/post/increase-likes/${post_id}`, {
+      method: "POST",
+    }).then(() => alert("좋아요 성공"));
+  };
+
+  postLikeButton.addEventListener("click", increateLike);
+}
+
+const postDeleteButton = document.getElementById("detail-post__delete-button");
+
+if (postDeleteButton) {
+  const deletePost = () => {
+    console.log("clicked");
+
+    const deleteConfirm = confirm("이 게시물을 삭제하시겠습니까?");
+    if (deleteConfirm) {
+      fetch(`/post/delete/${post_id}`, {
+        method: "POST",
+      }).then(() => {
+        alert("삭제 성공");
+      });
+    }
+  };
+
+  postDeleteButton.addEventListener("click", deletePost);
+}
