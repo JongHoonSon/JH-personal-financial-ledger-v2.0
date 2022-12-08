9bf6bd1804b2481628ef934627b731ce8eb9a1ae
JongHoonSon <roka1684@gmail.com>
2022-12-09 00:41:48 +0900

    fix: Edit loginRequiredPage and  anonymousUserPage message

--- src/middlewares/anonymousUserPage.js
+++ src/middlewares/anonymousUserPage.js
@@ -2,7 +2,7 @@ const anonymousUserPage = (req, res, next) => {
   if (!req.session.loggedIn) {
     next();
   } else {
-    req.flash("error", "잘못된 접근입니다.");
+    req.flash("error", "로그아웃이 필요한 페이지입니다.");
     return res.redirect("/");
   }
 };
--- src/middlewares/loginRequiredPage.js
+++ src/middlewares/loginRequiredPage.js
@@ -2,7 +2,7 @@ const loginRequiredPage = (req, res, next) => {
   if (req.session.loggedIn) {
     next();
   } else {
-    req.flash("error", "먼저 로그인을 해주세요.");
+    req.flash("error", "로그인이 필요한 페이지입니다.");
     return res.redirect("/login");
   }
 };
