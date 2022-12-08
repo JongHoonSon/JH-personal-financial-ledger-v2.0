b3e8b31f7d470000fbc8c8bd3afe786f762b363f
JongHoonSon <roka1684@gmail.com>
2022-12-08 14:18:49 +0900

    refactor: refactor db.js code

--- src/db.js
+++ src/db.js
@@ -7,8 +7,5 @@ mongoose.connect(process.env.DB_URL, {
 
 const db = mongoose.connection;
 
-const handleError = (error) => console.log("DB Error", error);
-const handleOpen = () => console.log("Connected to DB!");
-
-db.on("error", handleError);
-db.once("open", handleOpen);
+db.once("open", () => console.log("Connected to DB!"));
+db.on("error", (error) => console.log("DB Error", error));
