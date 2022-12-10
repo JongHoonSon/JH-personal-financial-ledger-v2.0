6b30cd3ee5a5b3257c16b08238158164161970a2
JongHoonSon <roka1684@gmail.com>
2022-12-11 00:59:58 +0900

    refactor: import loginRequired in index.js

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,6 +1,7 @@
 import localMiddleware from "./localMiddleware";
 import loginRequiredPage from "./loginRequiredPage";
 import loginRequiredAPI from "./loginRequiredAPI";
+import loginRequired from "./loginRequired";
 import anonymousUserPage from "./anonymousUserPage";
 import imageUploader from "./imageUploader";
 import logHistory from "./logHistory";
@@ -9,6 +10,7 @@ export {
   localMiddleware,
   loginRequiredPage,
   loginRequiredAPI,
+  loginRequired,
   anonymousUserPage,
   imageUploader,
   logHistory,
