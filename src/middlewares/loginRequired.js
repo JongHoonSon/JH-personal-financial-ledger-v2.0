d076ca5662dd61c994ec4101606ebff432e2b86d
JongHoonSon <roka1684@gmail.com>
2022-12-11 02:10:38 +0900

    refactor: Edit line formatting

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -2,12 +2,13 @@ const loginRequired = (req, res, next) => {
   if (req.session.loggedIn) {
     next();
   } else {
-    // 일반 페이지 접속에 대한 GET 요청
+    // 페이지 접속에 대한 GET 요청
     if (req.method === "GET" && req.headers["sec-fetch-dest"] === "document") {
       console.log("페이지에 대한 GET 요청");
       req.flash("error", "로그인한 유저만 접속 가능한 페이지입니다.");
       return res.redirect("/login");
     }
+
     // form으로 보낸 POST 요청
     else if (
       req.headers["content-type"] === "application/x-www-form-urlencoded"
@@ -16,6 +17,7 @@ const loginRequired = (req, res, next) => {
       req.flash("error", "먼저 로그인해 주세요.");
       return res.redirect("/login");
     }
+
     // fetch로 보낸 POST, PUT, DELETE 요청
     else if (
       req.headers["content-length"] === "0" ||
