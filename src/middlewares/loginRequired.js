957409df8243c5ccb4037b17634e99df558b968a
JongHoonSon <roka1684@gmail.com>
2022-12-11 01:54:18 +0900

    refactor: ADD logged incheck in PUT, DELETE method from fetch

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -16,10 +16,12 @@ const loginRequired = (req, res, next) => {
       req.flash("error", "먼저 로그인해 주세요.");
       return res.redirect("/login");
     }
-    // fetch로 보낸 POST 요청
+    // fetch로 보낸 POST, PUT, DELETE 요청
     else if (
       req.headers["content-length"] === "0" ||
-      req.headers["content-type"] === "application/json"
+      req.headers["content-type"] === "application/json" ||
+      req.method === "PUT" ||
+      req.method === "DELETE"
     ) {
       console.log("fetch로 보내는 POST 요청");
       req.flash("error", "먼저 로그인해 주세요.");
