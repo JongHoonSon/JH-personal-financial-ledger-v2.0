10490476664d09fa06894d73327b704ee54ffdbb
JongHoonSon <roka1684@gmail.com>
2022-12-14 18:48:07 +0900

    refactor: Edit detail-item class or id name

--- src/client/js/detail-item.js
+++ src/client/js/detail-item.js
@@ -1,4 +1,4 @@
-const deleteBtn = document.querySelector(".delete-icon");
+const deleteBtn = document.querySelector("#detail-item__delete-icon");
 
 function deleteConfirm(event) {
   if (!confirm("정말 이 내역을 삭제하시겠습니까?")) {
