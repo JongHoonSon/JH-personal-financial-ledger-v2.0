345475be688bb83f3e1a29dfab520d71664bbdbb
JongHoonSon <roka1684@gmail.com>
2023-02-01 00:22:13 +0900

    refactor: userRouter 구조 개선

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -5,19 +5,24 @@ import { imageUploader } from "../middlewares";
 const userRouter = express.Router();
 
 userRouter.get("/profile/:userId", userController.getUserProfile);
+
 userRouter
   .route("/edit-profile")
   .get(userController.getEditUserProfile)
   .put(imageUploader.single("image"), userController.editUserProfile);
+
 userRouter
   .route("/edit-password")
   .get(userController.getEditUserPassword)
   .put(userController.editUserPassword);
+
 userRouter
   .route("/category/:categoryType")
   .post(userController.addUserCategory)
   .delete(userController.deleteUserCategory);
+
 userRouter.get("/own-posts/:userId", userController.getUserOwnPosts);
+
 userRouter.get("/own-comments/:userId", userController.getUserOwnComments);
 
 export default userRouter;
