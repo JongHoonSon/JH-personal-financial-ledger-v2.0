50ed245a4b1cf5ff22afacb43938c52e76b56204
JongHoonSon <roka1684@gmail.com>
2023-02-01 00:15:55 +0900

    refactor: globalRouter 구조 개선

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -5,26 +5,28 @@ import passport from "../lib/passport.js";
 
 const globalRouter = express.Router();
 
-globalRouter.route("/").get(loginRequired, globalController.getHome);
+// Logged In User required
+globalRouter.get("/", loginRequired, globalController.getHome);
+globalRouter.get("/logout", loginRequired, globalController.logout);
+
+// Anonymous User Page
+globalRouter.use(anonymousUserPage);
 globalRouter
   .route("/join")
-  .get(anonymousUserPage, globalController.getJoin)
+  .get(globalController.getJoin)
   .post(globalController.join);
 globalRouter
   .route("/login")
-  .get(anonymousUserPage, globalController.getLogin)
+  .get(globalController.getLogin)
   .post(globalController.login);
 globalRouter.get(
   "/auth/google",
-  anonymousUserPage,
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 globalRouter.get(
   "/auth/google/callback",
-  anonymousUserPage,
   passport.authenticate("google"),
   globalController.finishGoogleLogin
 );
-globalRouter.get("/logout", loginRequired, globalController.logout);
 
 export default globalRouter;
