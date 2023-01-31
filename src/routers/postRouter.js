d08718e4f87ec730d79bb0570595408680f25ee7
JongHoonSon <roka1684@gmail.com>
2023-02-01 00:21:27 +0900

    refactor: postRouter 구조 개선

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -4,22 +4,19 @@ import { postController } from "../controllers";
 const postRouter = express.Router();
 
 postRouter.get("/add", postController.getAddPost);
+
 postRouter.post("/", postController.addPost);
+
 postRouter.get("/edit/:postId", postController.getEditPost);
+
 postRouter
   .route("/:postId")
   .get(postController.getDetailPost)
   .put(postController.editPost)
   .delete(postController.deletePost);
-postRouter.put(
-  "/increase-views/:postId",
 
-  postController.increasePostViews
-);
-postRouter.put(
-  "/toggle-likes/:postId",
+postRouter.put("/increase-views/:postId", postController.increasePostViews);
 
-  postController.togglePostLikes
-);
+postRouter.put("/toggle-likes/:postId", postController.togglePostLikes);
 
 export default postRouter;
