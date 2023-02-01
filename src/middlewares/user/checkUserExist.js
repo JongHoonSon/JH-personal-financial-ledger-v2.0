bd462a0d3ddf8ea40ee1c8fd5d9f715624fa63ff
JongHoonSon <roka1684@gmail.com>
2023-02-01 18:38:38 +0900

    refactor: async/await 추가

--- src/middlewares/user/checkUserExist.js
+++ src/middlewares/user/checkUserExist.js
@@ -1,11 +1,11 @@
 import { userModel } from "../db/models";
 
-const checkUserExist = (req, res, next) => {
+const checkUserExist = async (req, res, next) => {
   if (req.session.loggedIn) {
     const { user } = req.session;
 
     try {
-      const loggedInUser = userModel.findById(user._id);
+      const loggedInUser = await userModel.findById(user._id);
 
       if (!loggedInUser) {
         const error = new Error("로그인한 계정이 DB에 존재하지 않습니다.");
