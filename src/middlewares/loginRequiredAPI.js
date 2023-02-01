6ba555cd8fe0c72b77ddc6d8ba7fff2c199471c5
JongHoonSon <roka1684@gmail.com>
2023-02-01 16:49:50 +0900

    refactor: 안쓰이는 미들웨어 삭제

--- src/middlewares/loginRequiredAPI.js
+++
@@ -1,8 +0,0 @@
-export const loginRequiredAPI = (req, res, next) => {
-  if (req.session.loggedIn) {
-    next();
-  } else {
-    req.flash("error", "로그인이 필요한 API입니다.");
-    return res.status(401).json("/login");
-  }
-};
--- src/middlewares/loginRequiredPage.js
+++
@@ -1,8 +0,0 @@
-export const loginRequiredPage = (req, res, next) => {
-  if (req.session.loggedIn) {
-    next();
-  } else {
-    req.flash("error", "로그인이 필요한 페이지입니다.");
-    return res.redirect("/login");
-  }
-};
