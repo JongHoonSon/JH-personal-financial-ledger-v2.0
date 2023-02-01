c12a16c8e3db2210139f8cc560fe3d29a7ed4098
JongHoonSon <roka1684@gmail.com>
2023-02-01 18:25:19 +0900

    refactor: 미들웨어 이름 변경 anonymousUserPage -> checkUserAnonymous

---
+++ src/middlewares/checkUserAnonymous.js
@@ -0,0 +1,12 @@
+const checkUserAnonymous = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    next();
+  } else {
+    const error = new Error("로그아웃이 필요한 페이지입니다.");
+    error.statusCode = 401;
+    error.redirectURL = "/";
+    throw error;
+  }
+};
+
+export default checkUserAnonymous;
