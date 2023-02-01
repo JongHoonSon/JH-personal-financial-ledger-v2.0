52c231175686eed804e731315ee27f6e2143b482
JongHoonSon <roka1684@gmail.com>
2023-02-02 00:57:20 +0900

    refactor: owner check 미들웨어 구현

--- src/middlewares/comment/checkCommentOwner.js
+++ src/middlewares/comment/checkCommentOwner.js
@@ -1,5 +1,10 @@
-const checkCommentOwner = (req, res, next) => {
-  next();
+const checkCommentOwner = (comment, user, next) => {
+  if (String(comment.owner._id) === String(user._id)) {
+    const error = new Error("권한이 없습니다.");
+    error.statusCode = 403;
+    next(error);
+  }
+  return;
 };
 
 export default checkCommentOwner;
--- src/middlewares/item/checkItemOwner.js
+++ src/middlewares/item/checkItemOwner.js
@@ -1,5 +1,10 @@
-const checkItemOwner = (req, res, next) => {
-  next();
+const checkItemOwner = (item, user, next) => {
+  if (String(item.owner._id) === String(user._id)) {
+    const error = new Error("권한이 없습니다.");
+    error.statusCode = 403;
+    next(error);
+  }
+  return;
 };
 
 export default checkItemOwner;
