858e92783aa168fd771ba9c399923e73a4a9b74c
JongHoonSon <roka1684@gmail.com>
2023-02-02 01:54:55 +0900

    refactor: findOne 메서드 추가

--- src/db/models/userModel.js
+++ src/db/models/userModel.js
@@ -4,6 +4,22 @@ import { userSchema } from "../schemas";
 const User = mongoose.model("User", userSchema);
 
 class UserModel {
+  async findOne(params) {
+    try {
+      const user = await User.findOne(params);
+
+      if (!user) {
+        const error = new Error("유저를 DB에서 찾을 수 없습니다.");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async findById(userId) {
     try {
       const user = await User.findById(userId);
