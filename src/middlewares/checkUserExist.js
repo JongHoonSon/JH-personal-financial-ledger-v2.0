651119699fabebf9cfba50aee6fcc9c343d2ff71
JongHoonSon <roka1684@gmail.com>
2023-02-01 18:26:05 +0900

    refactor: 유저가 DB에 있는지 확인한 후 request session 객체에 저장

--- src/middlewares/checkUserExist.js
+++ src/middlewares/checkUserExist.js
@@ -2,17 +2,19 @@ import { userModel } from "../db/models";
 
 const checkUserExist = (req, res, next) => {
   if (req.session.loggedIn) {
-    const loggedInUser = req.session.user;
+    const { user } = req.session;
 
     try {
-      const isLoggedInUserExist = userModel.findById(loggedInUser._id);
+      const loggedInUser = userModel.findById(user._id);
 
-      if (!isLoggedInUserExist) {
+      if (!loggedInUser) {
         const error = new Error("로그인한 계정이 DB에 존재하지 않습니다.");
         error.statusCode = 401;
         error.redirectURL = "/";
         throw error;
       }
+
+      req.session.loggedInUser = loggedInUser;
     } catch (error) {
       throw error;
     }
