b945a1de3da2d6c6086d8371d65366af71392f30
JongHoonSon <roka1684@gmail.com>
2023-02-06 14:16:14 +0900

    refactor: 에러 메시지 변경

--- src/middlewares/common/checkParamNaN.js
+++ src/middlewares/common/checkParamNaN.js
@@ -1,6 +1,6 @@
 const checkParamNaN = (data, next) => {
   if (isNaN(data)) {
-    const error = new Error("잘못된 주소입니다.");
+    const error = new Error("잘못된 접근입니다.");
     error.statusCode = 400;
     error.redirectURL = `/404`;
     next(error);
