e56bda5221e9d1883a277f8bc1360896d352a746
JongHoonSon <roka1684@gmail.com>
2023-02-06 16:56:56 +0900

    deploy: s3 설정 변경

--- src/middlewares/common/imageUploader.js
+++ src/middlewares/common/imageUploader.js
@@ -3,6 +3,7 @@ import multerS3 from "multer-s3";
 import { S3Client } from "@aws-sdk/client-s3";
 
 const s3_client = new S3Client({
+  region: "ap-northeast-2",
   credentials: {
     accessKeyId: process.env.AWS_S3_ACCESS_ID,
     secretAccessKey: process.env.AWS_S3_ACCESS_KEY,
@@ -11,13 +12,12 @@ const s3_client = new S3Client({
 
 const s3_multer = multerS3({
   s3: s3_client,
-  bucket: "pflv2.0/images",
+  bucket: "pflv2.0",
 });
 
 const imageUploader = multer({
   dest: __dirname + "/../../../assets/img/user-upload-images/",
   storage: process.env.NODE_ENV === "production" ? s3_multer : undefined,
-  // storage: s3_multer,
 });
 
 export default imageUploader;
